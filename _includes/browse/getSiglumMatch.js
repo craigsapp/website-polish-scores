ffe73ab32400342becfa4be7259d060cde402bad
Craig Stuart Sapp <craigsapp@gmail.com>
2023-05-06 12:01:07 -0700

    Enhancement for issue https://github.com/craigsapp/website-polish-scores/issues/224

--- _includes/browse/getSiglumMatch.js
+++ _includes/browse/getSiglumMatch.js
@@ -2,7 +2,7 @@
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Sat May  6 11:23:21 PDT 2023
-// Last Modified: Sat May  6 11:23:23 PDT 2023
+// Last Modified: Sat May  6 11:58:02 PDT 2023
 // Filename:      _includes/browse/getSiglumMatch.js
 // Used by:
 // Included in:   _includes/browse/main.html
@@ -24,6 +24,7 @@ POPC2.prototype.getSiglumMatch = function (input, index) {
 	// Note: if there are accented keys in the full siglum ID,
 	// then these will have to be considered.
 	let fullKeys = Object.getOwnPropertyNames(this.VARS.BROWSE_MENU_OPTIONS.siglum);
+
 	for (let i=0; i<fullKeys.length; i++) {
 		let key = fullKeys[i];
 		if (key === input) {
@@ -33,6 +34,25 @@ POPC2.prototype.getSiglumMatch = function (input, index) {
 		}
 	}
 
+	// If a match has not been found, assume a PL- prefix:
+	for (let i=0; i<fullKeys.length; i++) {
+		let key = fullKeys[i];
+		if (key.toLowerCase() === "pl=" + input) {
+			return key;
+		}
+	}
+
+	// If a match has not been found, assume any country code prefix:
+	for (let i=0; i<fullKeys.length; i++) {
+		let key = fullKeys[i];
+		let searchkey = key.replace(/^[A-Za-z]+-/, "");
+		if (searchkey.toLowerCase() === input) {
+			return key;
+		}
+	}
+
+
+	// No matching siglum found.
 	return "";
 
 };

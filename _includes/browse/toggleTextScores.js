7d367adfb982d60e094aad8112a6f84bce8d8310
Craig Stuart Sapp <craigsapp@gmail.com>
2023-05-30 21:42:32 -0700

    Implementation for issue https://github.com/craigsapp/website-polish-scores/issues/227

--- _includes/browse/toggleTextScores.js
+++ _includes/browse/toggleTextScores.js
@@ -1,26 +1,26 @@
 {% comment %}
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
-// Creation Date: Sun Mar 19 20:08:51 PDT 2023
-// Last Modified: Sun Mar 19 20:08:54 PDT 2023
-// Filename:      _includes/browse/toggleModernScores.js
+// Creation Date: Tue May 30 21:29:55 PDT 2023
+// Last Modified: Tue May 30 21:29:59 PDT 2023
+// Filename:      _includes/browse/toggleTextScores.js
 // Used by:       
 // Included in:   _includes/browse/main.html
 // Syntax:        ECMAScript 6
 // vim:           ts=3:nowrap
 //
-// Description:   Toggle displaying only modernized scores
+// Description:   Toggle displaying only scores containing text (lyrics).
 //
 {% endcomment %}
 
-POPC2.prototype.toggleIiifScores = function () {
+POPC2.prototype.toggleTextScores = function () {
 	this.DebugMessageFunction();
 
 	if (popc2.IsBrowsePage()) {
-		popc2.VARS.SEARCH_FLAGS.IIIF = !popc2.VARS.SEARCH_FLAGS.IIIF;
-		let element = document.querySelector("#iiif-browse-button");
+		popc2.VARS.SEARCH_FLAGS.TEXT = !popc2.VARS.SEARCH_FLAGS.TEXT;
+		let element = document.querySelector("#text-browse-button");
 		if (element) {
-			if (popc2.VARS.SEARCH_FLAGS.IIIF) {
+			if (popc2.VARS.SEARCH_FLAGS.TEXT) {
 				element.classList.add("highlight");
 			} else {
 				element.classList.remove("highlight");
@@ -31,16 +31,18 @@ POPC2.prototype.toggleIiifScores = function () {
 			let element = document.querySelector("#modern-browse-button");
 			element.classList.remove("highlight");
 		}
-		if (popc2.VARS.SEARCH_FLAGS.TEXT) {
-			popc2.VARS.SEARCH_FLAGS.TEXT = false;
-			let element = document.querySelector("#text-browse-button");
+		if (popc2.VARS.SEARCH_FLAGS.IIIF) {
+			popc2.VARS.SEARCH_FLAGS.IIIF = false;
+			let element = document.querySelector("#iiif-browse-button");
 			element.classList.remove("highlight");
 		}
 		popc2.doBrowseSearch();
 	}
 };
 
-Object.defineProperty(POPC2.prototype.toggleIiifScores, "name", { value: "toggleTextScores" });
+Object.defineProperty(POPC2.prototype.toggleTextScores, "name", { value: "toggleTextScores" });
+
+
 
 
 

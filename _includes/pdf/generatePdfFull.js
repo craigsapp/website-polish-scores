6591dd4d60509ac60cbcc4a7665c4d30c966905d
Craig Stuart Sapp <craigsapp@gmail.com>
2023-08-11 14:25:22 +0200

    Remove duplicate filter.

--- _includes/pdf/generatePdfFull.js
+++ _includes/pdf/generatePdfFull.js
@@ -109,7 +109,7 @@ POPC2.prototype.generatePdfFull = function (format, orientation) {
 	}
 
 	let scoredata = this.VARS.HUMDRUM[this.VARS.WORK_ID];
-	scoredata += "\n!!!filter: shed -e s/t=P://L | shed -e s/:problem/:PROBLEM/L\n";
+	// scoredata += '\n!!!filter: shed -e "^LO:TX.*:problem\\b.*//L"\n';
 	// scoredata += "!!!header-center: " + HUMDRUMFILETITLE + "\n";
 	scoredata += "!!!header-left: \n";
 	//scoredata += "!!!header-left: @{PPR}, @{PPP}\n";
